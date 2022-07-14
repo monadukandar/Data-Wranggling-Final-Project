609dcc931b05aa0b0eba7cfc04a785b83438737f
monadukandar <104590400+monadukandar@users.noreply.github.com>
2022-07-14 15:13:36 +0530

    Update Data Wranngling FinalProject.py

--- Data Wranngling FinalProject.py
+++ Data Wranngling FinalProject.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
+
 
 # # Title: Problem for Covid - 19 Data Analysis Project using Python
 
-# In[5]:
+
 
 
 import numpy as np
@@ -12,7 +11,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# In[6]:
 
 
 URL = "https://raw.githubusercontent.com/SR1608/Datasets/main/covid-data.csv"
@@ -20,13 +18,11 @@ URL = "https://raw.githubusercontent.com/SR1608/Datasets/main/covid-data.csv"
 
 # # 1. Import the dataset using Pandas from above mentioned url.
 
-# In[7]:
 
 
 df = pd.read_csv( "https://raw.githubusercontent.com/SR1608/Datasets/main/covid-data.csv")
 
 
-# In[8]:
 
 
 df.head()
@@ -36,19 +32,19 @@ df.head()
 
 # ## a. Find no. of rows & columns in the dataset
 
-# In[10]:
+
 
 
 df.shape
 
 
-# In[9]:
+
 
 
 print(f"No of rows : {df.shape[0]}")
 
 
-# In[10]:
+
 
 
 print(f"No of columns : {df.shape[1]}")
@@ -56,7 +52,7 @@ print(f"No of columns : {df.shape[1]}")
 
 # ## b. Data types of columns.
 
-# In[11]:
+
 
 
 df.dtypes
@@ -64,13 +60,13 @@ df.dtypes
 
 # ## c. Info & describe of data in dataframe.
 
-# In[12]:
+
 
 
 df.info()
 
 
-# In[13]:
+
 
 
 df.describe()
@@ -80,7 +76,7 @@ df.describe()
 
 # ## a. Find count of unique values in location column.
 
-# In[14]:
+
 
 
 df["location"].nunique()
@@ -88,7 +84,7 @@ df["location"].nunique()
 
 # ## b. Find which continent has maximum frequency using values counts.
 
-# In[15]:
+
 
 
 df["continent"].value_counts()
@@ -96,19 +92,18 @@ df["continent"].value_counts()
 
 # ## c. Find maximum & mean value in 'total_cases'.
 
-# In[16]:
+
 
 
 df["total_cases"].max()
 
 
-# In[17]:
+
 
 
 df["total_cases"].min()
 
 
-# In[18]:
 
 
 df["total_cases"].mean()
@@ -116,19 +111,19 @@ df["total_cases"].mean()
 
 # ## d. Find 25%,50% & 75% quartile value in 'total_deaths'.
 
-# In[19]:
+
 
 
 df["total_deaths"].quantile(0.25)
 
 
-# In[20]:
+
 
 
 df["total_deaths"].quantile(0.5)
 
 
-# In[21]:
+
 
 
 df["total_deaths"].quantile(0.75)
@@ -136,7 +131,7 @@ df["total_deaths"].quantile(0.75)
 
 # ## e. Find which continent has maximum 'human_development_index'.
 
-# In[22]:
+
 
 
 df.iloc[df["human_development_index"].idxmax()]["continent"]
@@ -144,7 +139,7 @@ df.iloc[df["human_development_index"].idxmax()]["continent"]
 
 # ## f. Find which continent has minimum 'gdp_per_capita'.
 
-# In[23]:
+
 
 
 df.iloc[df["gdp_per_capita"].idxmin()]["continent"]
@@ -152,13 +147,13 @@ df.iloc[df["gdp_per_capita"].idxmin()]["continent"]
 
 # # 4. Filter the dataframe with only this columns ['continent','location','date','total_cases','total_deaths','gdp_per_capita','human_development_index'] and update the data frame.
 
-# In[24]:
+
 
 
 df = df[['continent','location','date','total_cases','total_deaths','gdp_per_capita','human_development_index']]
 
 
-# In[25]:
+
 
 
 df.head()
@@ -168,13 +163,13 @@ df.head()
 
 # ## a. Remove all duplicates observations
 
-# In[26]:
+
 
 
 df.drop_duplicates(inplace=True)
 
 
-# In[27]:
+
 
 
 df.shape
@@ -182,7 +177,6 @@ df.shape
 
 # ## b. Find missing values in all columns
 
-# In[28]:
 
 
 df.isnull().sum()
@@ -190,13 +184,13 @@ df.isnull().sum()
 
 # ## c. Remove all observations where continent column value is missing
 
-# In[29]:
+
 
 
 df.dropna(subset="continent",inplace= True)
 
 
-# In[30]:
+
 
 
 df.shape
@@ -204,7 +198,7 @@ df.shape
 
 # ## d. Fill all missing values with 0
 
-# In[31]:
+
 
 
 df.fillna(value=0,inplace=True)
@@ -214,7 +208,7 @@ df.fillna(value=0,inplace=True)
 
 # ## a. Convert date column in datetime format using pandas.to_datetime
 
-# In[32]:
+
 
 
 pd.to_datetime(df["date"])
@@ -222,13 +216,13 @@ pd.to_datetime(df["date"])
 
 # ## b. Create new column month after extracting month data from date column.
 
-# In[33]:
+
 
 
 df["month"] = pd.to_datetime(df["date"]).dt.month
 
 
-# In[34]:
+
 
 
 df.head()
@@ -238,7 +232,7 @@ df.head()
 
 # ## a. Find max value in all columns using groupby function on 'continent' column
 
-# In[35]:
+
 
 
 df.groupby("continent").max().reset_index()
